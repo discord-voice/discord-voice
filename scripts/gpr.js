11fc5a932040b950c815d456f8168603656b19f2
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-03-25 20:50:09 +0530

    Add files via upload

---
+++ scripts/gpr.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const { join } = require('path');
+
+const pkg = require('../package.json');
+
+pkg.name = `@Lebyy/${pkg.name}`;
+
+fs.writeFileSync(join(__dirname, '../package.json'), JSON.stringify(pkg));
