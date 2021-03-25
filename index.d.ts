ce4518553b625046746d41239fdc7ba0c29814bc
Lebyy Dev <lebon1377@gmail.com>
2021-03-25 15:27:50 +0000

    Updating typings

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,16 @@
 export class DiscordVoice{
 
-public setURL(dbUrl:string);
-public createUser(userId:string, guildId:string);
-public deleteUser(userId:string, guildId:string);
-public start(client:any, trackbots:boolean, trackallchannels:boolean, channelID:string);
-public setVoiceTime(userId:string, guildId:string, voicetime:number);
-public fetch(userId:string, guildId:string, fetchPosition:boolean);
-public addVoiceTime(userId:string, guildId:string, voicetime:number);
-public subtractVoiceTime(userId:string, guildId:string, voicetime:number);
-public fetchLeaderboard(guildId:string, limit:number);
-public computeLeaderboard(client:any, leaderboard:any, fetchUsers:boolean);
+public setURL(dbUrl:string): object;
+public createUser(userId:string, guildId:string): object;
+public deleteUser(userId:string, guildId:string): object;
+public start(client:any, trackbots:boolean, trackallchannels:boolean, userlimit:number, channelID:string): object;
+public setVoiceTime(userId:string, guildId:string, voicetime:number): object;
+public fetch(userId:string, guildId:string, fetchPosition:boolean): object;
+public addVoiceTime(userId:string, guildId:string, voicetime:number): object;
+public subtractVoiceTime(userId:string, guildId:string, voicetime:number): object;
+public resetGuild(guildId:string): object;
+public fetchLeaderboard(guildId:string, limit:number): object;
+public computeLeaderboard(client:any, leaderboard:any, fetchUsers:boolean): object;
+public blacklist(userId:string, guildId:string): object;
+public unblacklist(userId:string, guildId:string): object;
 }
