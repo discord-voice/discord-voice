de789f0106d3bbcce1741b6fef42761522bb1e08
Lebyy Dev <>
2021-04-04 08:29:40 +0000

    Adding checks for unique data

--- models/voice.js
+++ models/voice.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-
+const uniqueValidator = require('mongoose-unique-validator')
 const VoiceSchema = new mongoose.Schema({
-  userID: { type: String },
+  userID: { type: String, unique: true },
   guildID: { type: String },
 	joinTime: { type: Object, default: {} },
   voiceTime: { type: Object, default: {} },
 	isBlacklisted: { type: Boolean, default: false },
   lastUpdated: { type: Date, default: new Date() }
 });
-
+VoiceSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Voice', VoiceSchema);
