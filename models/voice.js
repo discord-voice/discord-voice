c337fe1e3840fb88a9a1d954242166a43ba1f63c
Lebyy Dev <>
2021-04-10 16:49:47 +0000

    Updating mongoose version

--- models/voice.js
+++ models/voice.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require('mongoose-unique-validator')
 const VoiceSchema = new mongoose.Schema({
-  userID: { type: String, unique: true },
+  userID: { type: String },
   guildID: { type: String },
 	joinTime: { type: Object, default: {} },
   voiceTime: { type: Object, default: {} },
 	isBlacklisted: { type: Boolean, default: false },
   lastUpdated: { type: Date, default: new Date() }
 });
-VoiceSchema.plugin(uniqueValidator)
 module.exports = mongoose.model('Voice', VoiceSchema);
