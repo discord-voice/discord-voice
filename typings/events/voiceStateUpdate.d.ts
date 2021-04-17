c5e68d088600e67129f71e0ddfaf79dc938022f0
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-04-17 12:02:20 +0530

    ✏Updating typings

--- typings/events/voiceStateUpdate.d.ts
+++ typings/events/voiceStateUpdate.d.ts
@@ -1,2 +1,2 @@
 import { Client, VoiceState } from 'discord.js';
-export function execute(client: Client, oldState: VoiceState, newState: VoiceState, Voice: any, VoiceConfig: any, event: any): Promise<any>;
+export function execute(client: Client, oldState: VoiceState, newState: VoiceState, Voice: any, VoiceConfig: any, event: any): Promise<void>;
