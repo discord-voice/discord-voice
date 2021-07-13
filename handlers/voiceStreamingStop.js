29b1b5133792e690a8b4728be9eed6072bc58c8c
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-07-13 21:42:39 +0530

    ✏️Deleting not required files

--- handlers/voiceStreamingStop.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  execute: async(client, member, channel, Voice, VoiceConfig, emit) => {
-  return; // Coming soon next month maybe?
-	}
-}
--- typings/handlers/voiceChannelUndeaf.d.ts
+++
@@ -1,1 +0,0 @@
-export function execute(client: any, member: any, oldDeafType: any, Voice: any, VoiceConfig: any, emit: any): Promise<any>;
